27004ceb1aa2362f4a22b6dda0d4c3b981fc7d53
renanzim1 <weslerenan91@gmail.com>
2026-08-19 13:11:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // PREENCHA DEPOIS com os dados do Supabase.
 window.FOTOFLOW_CONFIG = {
-  supabaseUrl: "https://gcypuolqddimauhtsrgzg.supabase.co",
+  supabaseUrl: "https://gcypuolqdimauhtsrgzg.supabase.co/rest/v1/",
   supabaseAnonKey: "sb_publishable_L_IqpLeyJfKjG2PuI-RRWw__eBX-Bmo",
   studioName: "Fotoflow",
   adminPassword: "960867"
