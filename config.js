4bb9e9f6f6f0241df16fe98f0f4d8dedcc01fbe9
renanzim1 <weslerenan91@gmail.com>
2026-08-19 08:04:13 -0300

    Foto1.0

---
+++ config.js
@@ -0,0 +1,7 @@
+// PREENCHA DEPOIS com os dados do Supabase.
+window.FOTOFLOW_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  studioName: "Fotoflow",
+  adminPassword: "troque-esta-senha"
+};
